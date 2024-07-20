28f7dd75761c268c152652934d83c7a1f506fbcb
Victory Emmanuel <marketinglot.blog@gmail.com>
2024-07-20 15:03:58 +0100

    editing page

--- src/pages/editing.jsx
+++ src/pages/editing.jsx
@@ -283,110 +283,6 @@ const Editing = () => {
             </Link>
           </div>
         </section>
-        {/*   Books I've Worked On */}
-        <div className="  mx-auto px-6 py-16 mb-8 md:px-12">
-          <div className="text-center">
-            <h2
-              style={{
-                borderBottom: "2px solid #343434",
-                paddingBottom: "1rem",
-              }}
-              className=" mb-12 text-center text-2xl xx:hidden ss:inline-block font-bold text-secondary dark:text-white md:text-4xl"
-            >
-              Books I've Worked On
-            </h2>
-          </div>
-
-          <div className="grid gap-6 ss:px-4 xx:px-0 sm:px-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-            <div className="group relative rounded-3xl xx:w-full ss:space-y-6 overflow-hidden">
-              <img
-                className="mx-auto h-full w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://i.postimg.cc/wjLvVbsj/20240218-134019.jpg"
-                alt="woman"
-                loading="lazy"
-                width="640"
-                height="805"
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4  className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
-                                  Precious Ayomikun
-                  </h4>
-                </div>
-                 <p className="mt-2 text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
-                    Precious Ayomikun carries the girl child in her thoughts —
-                    in things she does, with the projects she embarks on, and
-                    the places she volunteers in...{" "}
-                    <HashLink smooth to="/editing#a-gift-to-deborah">
-                      <span className="text-semibold text-primary">
-                        See More
-                      </span>
-                    </HashLink>
-                  </p>
-              </div>
-            </div>
-
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <img
-                className="mx-auto h-full w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://i.postimg.cc/xCxTCwBt/20240218-134201.jpg"
-                alt="Book cover"
-                loading="lazy"
-                width="640"
-                height="805"
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4  className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
-                            Stephen Akintayo
-                  </h4>
-                </div>
-                <p className="mt-2 text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
-                    The personnel in charge of getting Dr. Stephen Akintayo's
-                    Billionaire Codes edited sought an editor who could cut out
-                    repetitions, tighten every chapter, and sharpen the message.
-                    ...
-                    <HashLink smooth to="/editing#billionaire-codes">
-                      <span className="text-semibold text-primary">
-                        See More
-                      </span>
-                    </HashLink>
-                  </p>
-              </div>
-            </div>
-
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <img
-                className="mx-auto h-full w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://i.postimg.cc/j2Bx6zxf/20240218-134232.jpg"
-                alt="woman"
-                loading="lazy"
-                width="640"
-                height="805"
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                    <h4 className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
-                      Mayowa Owolabi
-                    </h4>
-                  </div>
-                  <p className=" text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
-                    When this manuscript got to me, I was told it had been
-                    edited already. I'll never be caught putting down another
-                    person's work but to be professionally fair, this
-                    manuscript, supposedly edited before getting to me,
-                    surprised me...
-                    <HashLink smooth to="/editing#edusell">
-                      <span className="text-semibold text-primary">
-                        See More
-                      </span>
-                    </HashLink>
-                  </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
         {/* My services */}
         <section className="">
           <div className="text-center my-12">
@@ -630,6 +526,103 @@ const Editing = () => {
             </div>
           </div>
         </section>
+        {/*   Books I've Worked On */}
+        <div className="  mx-auto px-6 py-16 mb-8 md:px-12">
+          <div className="text-center">
+            <h2
+              style={{
+                borderBottom: "2px solid #343434",
+                paddingBottom: "1rem",
+              }}
+              className=" mb-12 text-center text-2xl xx:hidden ss:inline-block font-bold text-secondary dark:text-white md:text-4xl"
+            >
+              Books I've Worked On
+            </h2>
+          </div>
+
+          <div className="grid gap-6 ss:px-4 xx:px-0 sm:px-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+            <div className="group relative rounded-3xl xx:w-full ss:space-y-6 overflow-hidden">
+              <img
+                className="mx-auto h-full w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
+                src="https://i.postimg.cc/wjLvVbsj/20240218-134019.jpg"
+                alt="woman"
+                loading="lazy"
+                width="640"
+                height="805"
+              />
+              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                <div>
+                  <h4 className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
+                    Precious Ayomikun
+                  </h4>
+                </div>
+                <p className="mt-2 text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
+                  Precious Ayomikun carries the girl child in her thoughts — in
+                  things she does, with the projects she embarks on, and the
+                  places she volunteers in...{" "}
+                  <HashLink smooth to="/editing#a-gift-to-deborah">
+                    <span className="text-semibold text-primary">See More</span>
+                  </HashLink>
+                </p>
+              </div>
+            </div>
+
+            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+              <img
+                className="mx-auto h-full w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
+                src="https://i.postimg.cc/xCxTCwBt/20240218-134201.jpg"
+                alt="Book cover"
+                loading="lazy"
+                width="640"
+                height="805"
+              />
+              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                <div>
+                  <h4 className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
+                    Stephen Akintayo
+                  </h4>
+                </div>
+                <p className="mt-2 text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
+                  The personnel in charge of getting Dr. Stephen Akintayo's
+                  Billionaire Codes edited sought an editor who could cut out
+                  repetitions, tighten every chapter, and sharpen the message.
+                  ...
+                  <HashLink smooth to="/editing#billionaire-codes">
+                    <span className="text-semibold text-primary">See More</span>
+                  </HashLink>
+                </p>
+              </div>
+            </div>
+
+            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+              <img
+                className="mx-auto h-full w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0"
+                src="https://i.postimg.cc/j2Bx6zxf/20240218-134232.jpg"
+                alt="woman"
+                loading="lazy"
+                width="640"
+                height="805"
+              />
+              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-accent dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                <div>
+                  <h4 className="text-xl xx:mb-2  ss:mb-4 font-semibold dark:text-gray-700 text-white">
+                    Mayowa Owolabi
+                  </h4>
+                </div>
+                <p className=" text-primary dark:text-gray-600 text-base xs:text-sm xx:text-xs">
+                  When this manuscript got to me, I was told it had been edited
+                  already. I'll never be caught putting down another person's
+                  work but to be professionally fair, this manuscript,
+                  supposedly edited before getting to me, surprised me...
+                  <HashLink smooth to="/editing#edusell">
+                    <span className="text-semibold text-primary">See More</span>
+                  </HashLink>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* STEPS */}
         <div className="ss:px-12 xx:px-6 mt-12 py-16 mb-12 mx-auto ">
           <div className="mb-12 md:mx-auto text-center">
